162d73016235cf545477d9bdd93f87027851213d
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-22 18:24:44 -0400

    Media file handles the storage of the image

--- routes/media.js
+++ routes/media.js
@@ -1,6 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var path = require('path');
+var multer  = require('multer');
+
+var storage = multer.diskStorage(
+	{
+	destination: './temp/',
+  	filename: function (req, file, cb) {
+	    crypto.pseudoRandomBytes(16, function (err, raw) {
+	      if (err) return cb(err)
+
+	      cb(null, raw.toString('hex') + path.extname(file.originalname))
+	    });
+	    
+  	}
+});
+
+var upload = multer({ storage: storage , limits: {fileSize: 16000000 }}); //16MB File Limit
+var middleware = upload.single('mediaFile');
+
+router.post('/create' ,function(req, res, next) {
+	middleware(req , res, function(err){
+		console.dir(req.body);
+		res.send(req.body);
+	});
+});
+
+
 router.post('/upload', function(req, res, next){
 	res.send(req);
 });
