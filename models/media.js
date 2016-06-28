eeffaa835967be3139e0ccaf19102f20f118e447
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-27 22:02:04 -0400

    Updated Media model, to better represent media that is being stored, and set the JSON Export.

--- models/media.js
+++ models/media.js
@@ -3,19 +3,33 @@ var Schema = mongoose.Schema;
 
 
 var mediaSchema = Schema({
-	image: Buffer,
+	date: {type: Date , default: Date.now },
+	image: { data: { type: Buffer, required: true },
+					contentType: { type: String , required: true}
+				},
 	tags: [String],
-	comments: [String],
+	comments: { type: [String] , default: [] },
 	approved: { type: Boolean , default: true },
+	//User not implemented yet
 	createdBy: {
 		type: Schema.Types.ObjectId,
 		ref: 'user'
 	},
 	party: {
 		type: Schema.Types.ObjectId,
-		ref: 'party'
+		ref: 'party',
+		required: true
 	}
 });
 
+if (!mediaSchema.options.toJSON) {
+  mediaSchema.options.toJSON = {};
+  mediaSchema.options.toJSON.transform = function (doc, ret, options) {
+  // remove the _id of every document before returning the result
+  delete ret.image;
+  delete ret._id;
+  delete ret.__v;
+  }
+}
 
 module.exports = mongoose.model('media' , mediaSchema);
